ecff1746af7690f6b3e57dbd51928c94205036de
makishima98 <54191137+makishima98@users.noreply.github.com>
2020-05-21 00:33:23 +0200

    burger-button

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,32 @@ $(document).ready(function(){
     $(`.owl-prev`).html(`<i class="fas fa-angle-left"></i>`)
     
   });
+
+let burgerButton = document.getElementsByClassName(`burger-button`);
+let navbar = document.getElementsByClassName(`navbar`);
+let buttonContainer = document.getElementsByClassName(`button-container`);
+let sections = document.querySelectorAll(`section`)
+let footer = document.querySelectorAll(`footer`)
+let list = document.getElementsByClassName(`nav-list`)[0];
+
+function classToggle(elements, classElement) {
+    for (let element of elements) {
+        element.classList.toggle(`${classElement}`)
+    }
+}
+
+burgerButton[0].addEventListener(`click`, function() {
+    classToggle(burgerButton, `hidden`);
+    classToggle(navbar, `hidden-nav`);
+    classToggle(buttonContainer, `active`)
+    // classToggle(sections, `display-none`)
+    // classToggle(footer, `display-none`)
+})
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener(`click`, function(){
+        classToggle(burgerButton, `hidden`);
+        classToggle(navbar, `hidden-nav`);
+        classToggle(buttonContainer, `active`)
+    })    
+}
