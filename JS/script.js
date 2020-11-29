527c30aa5f7cbc6c7eba20422e92f095aaa08f75
makishima98 <54191137+makishima98@users.noreply.github.com>
2020-11-29 13:02:20 +0100

    finished

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         loop:true,
         responsiveClass:true,
         nav:true,
-        autoWidth: true
+        autoWidth: true,
     })
     $(`.owl-next`).html(`<i class="fas fa-angle-right"></i>`)
     $(`.owl-prev`).html(`<i class="fas fa-angle-left"></i>`)
@@ -17,7 +17,7 @@ let navbar = document.getElementsByClassName(`navbar`);
 let buttonContainer = document.getElementsByClassName(`button-container`);
 let sections = document.querySelectorAll(`section`)
 let footer = document.querySelectorAll(`footer`)
-let list = document.getElementsByClassName(`nav-list`)[0];
+let list = document.getElementsByClassName(`nav-list`)[0].children;
 
 function classToggle(elements, classElement) {
     for (let element of elements) {
@@ -25,12 +25,18 @@ function classToggle(elements, classElement) {
     }
 }
 
+function classRemove(elements, classElement) {
+    for (let element of elements) {
+        element.classList.remove(`${classElement}`)
+    }
+}
+
 burgerButton[0].addEventListener(`click`, function() {
     classToggle(burgerButton, `hidden`);
     classToggle(navbar, `hidden-nav`);
     classToggle(buttonContainer, `active`)
-    // classToggle(sections, `display-none`)
-    // classToggle(footer, `display-none`)
+    classToggle(sections, `display-none`)
+    classToggle(footer, `display-none`)
 })
 
 for (let i = 0; i < list.length; i++) {
@@ -38,5 +44,7 @@ for (let i = 0; i < list.length; i++) {
         classToggle(burgerButton, `hidden`);
         classToggle(navbar, `hidden-nav`);
         classToggle(buttonContainer, `active`)
+        classRemove(sections, `display-none`)
+        classRemove(footer, `display-none`) 
     })    
 }
